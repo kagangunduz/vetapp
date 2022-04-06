57a7ede714e5001df00e2d4bec9d74e40666ba1d
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-06 13:54:14 +0300

    pom.xml'e ModelMapper eklendi ve bean olarak oluşturuldu.

--- src/main/java/com/kagangunduz/vet/VetApplication.java
+++ src/main/java/com/kagangunduz/vet/VetApplication.java
@@ -1,7 +1,9 @@
 package com.kagangunduz.vet;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VetApplication {
@@ -9,5 +11,11 @@ public class VetApplication {
     public static void main(String[] args) {
         SpringApplication.run(VetApplication.class, args);
     }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
 
 }
