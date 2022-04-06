7b22bb8e94441177669daa91eb8fc4f547483b3b
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-06 11:47:50 +0300

    BaseEntity oluşturuldu.

---
+++ src/main/java/com/kagangunduz/vet/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.kagangunduz.vet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity implements Serializable {
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    private Boolean status;
+
+}
