c952ae09ad6ab0961faf0b97a14938fc9912f53f
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-08 00:12:42 +0300

    Exception'lar için genel yapı oluşturuldu.
    GlobalExceptionHandler oluşturuldu, PetNotFoundException oluşturuldu ve exception için error sayfası oluşturuldu.

---
+++ src/main/java/com/kagangunduz/vet/advice/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.kagangunduz.vet.advice;
+
+import com.kagangunduz.vet.exception.PetNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(PetNotFoundException.class)
+    public ModelAndView handlePetNotFoundException(PetNotFoundException ex, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+    }
+
+    /*@ExceptionHandler(EntityNotFoundException.class)
+    public ModelAndView handleEntityNotFoundException(EntityNotFoundException ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+    }*/
+
+
+}
