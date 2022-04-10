27127154efd23ab435742d94515a82fa146e28e3
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-10 14:26:34 +0300

    EntityNotFoundException eklendi.

--- src/main/java/com/kagangunduz/vet/advice/GlobalExceptionHandler.java
+++ src/main/java/com/kagangunduz/vet/advice/GlobalExceptionHandler.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({PetNotFoundException.class, OwnerNotFoundException.class})
+    @ExceptionHandler({PetNotFoundException.class, OwnerNotFoundException.class, EntityNotFoundException.class})
     public ModelAndView handlePetNotFoundException(PetNotFoundException ex, HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", ex.getMessage());
