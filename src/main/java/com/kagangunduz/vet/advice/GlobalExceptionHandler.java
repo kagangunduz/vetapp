49b2cdded9a921be33b6a417228bfc94cb945c85
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-12 11:42:20 +0300

    GlobalExceptionHandler düzenlendi.

--- src/main/java/com/kagangunduz/vet/advice/GlobalExceptionHandler.java
+++ src/main/java/com/kagangunduz/vet/advice/GlobalExceptionHandler.java
@@ -1,31 +1,18 @@
 package com.kagangunduz.vet.advice;
 
-import com.kagangunduz.vet.exception.OwnerNotFoundException;
-import com.kagangunduz.vet.exception.PetNotFoundException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({PetNotFoundException.class, OwnerNotFoundException.class, EntityNotFoundException.class})
-    public ModelAndView handlePetNotFoundException(PetNotFoundException ex, HttpServletRequest request) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("message", ex.getMessage());
-        return modelAndView;
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handlePetNotFoundException(Model model, EntityNotFoundException ex) {
+        model.addAttribute("message", ex.getMessage());
+        return "error";
     }
-
-    /*@ExceptionHandler(EntityNotFoundException.class)
-    public ModelAndView handleEntityNotFoundException(EntityNotFoundException ex) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("error");
-        modelAndView.addObject("message", ex.getMessage());
-        return modelAndView;
-    }*/
-
-
+    
 }
