c5ebe59b576532df124f557d9e83fe04f51becd5
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-14 22:33:49 +0300

    Homepage controller ve html sayfası düzenlendi.

--- src/main/java/com/kagangunduz/vet/controller/HomepageController.java
+++ src/main/java/com/kagangunduz/vet/controller/HomepageController.java
@@ -1,15 +1,26 @@
 package com.kagangunduz.vet.controller;
 
+import com.kagangunduz.vet.service.impl.OwnerServiceImpl;
+import com.kagangunduz.vet.service.impl.PetServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HomepageController {
 
+    private final OwnerServiceImpl ownerService;
+    private final PetServiceImpl petService;
+
+
     @GetMapping
-    public String showHomepage() {
+    public String showHomepage(Model model) {
+        model.addAttribute("ownerCount", ownerService.getOwnerCount());
+        model.addAttribute("petCount", petService.getPetCount());
         return "homepage";
     }
 }
