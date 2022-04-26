c5e3abe9bfa01c341d8eae93758da922bf47fb91
Kağan Gündüz <kagangunduz94@gmail.com>
2022-04-27 00:06:40 +0300

    GlobalExceptionHandler güncellendi.

---
+++ src/main/java/com/kagangunduz/vet/exception/GenusNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kagangunduz.vet.exception;
+
+public class GenusNotFoundException extends RuntimeException {
+    public GenusNotFoundException(String message) {
+        super(message);
+    }
+}
